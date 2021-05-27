e2a9b274ac961645bdfe40887fddcb820c76393d
Debikapaul <82105390+Debikapaul@users.noreply.github.com>
2021-05-27 14:24:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ ctx = canvas.getContext("2d");
 
 car1_width= 120;
 car1_height=70;
-car1_image=car1.png;
+car1_image="car1.png";
 car1_x=10;
 car1_y=10;
 
 car2_width= 120;
 car2_height=70;
-car2_image=car2.png;
+car2_image="car2.png";
 car2_x=10;
-car2_y=10;
+car2_y=100;
 
 background_image="racing1.jpg";
 
@@ -50,27 +50,51 @@ function my_keydown(e){
     if(keyPressed=='38')
     {
         car1_up();
-        console.log("up");
+        console.log("up arrow key");
     }
 
     if(keyPressed=='40')
     {
         car1_down();
-        console.log("down");
+        console.log("down arrow key");
     }
 
     if(keyPressed=='37')
     {
         car1_left();
-        console.log("left");
+        console.log("left arrow key");
     }
 
     if(keyPressed=='39')
     {
        car1_right();
-        console.log("right");
+        console.log("right arrow key");
     }
 }
+if(keyPressed=='87')
+{  
+car2_up();
+console.log("key w");
+}
+
+if(keyPressed=='83')
+{
+car2_down();
+console.log("key s");
+}
+
+if(keyPressed=='65')
+{
+car2_left();
+console.log("key a");
+}
+
+if(keyPressed=='68')
+{
+car2_right();
+console.log("key d");
+}
+
 
 function up(){
     if(car1_y >=0){
